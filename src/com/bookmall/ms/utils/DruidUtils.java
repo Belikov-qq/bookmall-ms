7d0e8235a2af2ab3dbd0f2d8834315cbcc3eb605
Zhaoqi LI <1517705600@qq.com>
2022-05-03 22:37:18 +0800

    创建项目——2022年5月3日 22点37分 lqf

--- src/com/bookmall/ms/utils/DruidUtils.java
+++ src/com/bookmall/ms/utils/DruidUtils.java
@@ -4,6 +4,8 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidDataSourceFactory;
 
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DruidUtils {
@@ -20,5 +22,18 @@ public class DruidUtils {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 获取数据库连接池数据源对象
+     */
+    public static Connection getConnection(){
+        Connection connection = null;
+        try{
+            connection = druidDataSource.getConnection();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
