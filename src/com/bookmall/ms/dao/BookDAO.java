6dbea5d6900c766c3885ada4d52a98b67ebccf29
Lightmarey <lightmarey007@gmail.com>
2022-05-06 16:08:42 +0800

    分页查询语句修改 增加查询总数函数

--- src/com/bookmall/ms/dao/BookDAO.java
+++ src/com/bookmall/ms/dao/BookDAO.java
@@ -4,6 +4,7 @@ import com.bookmall.ms.dto.Book;
 import com.bookmall.ms.utils.DruidUtils;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -33,18 +34,39 @@ public class BookDAO {
 
     /**
      * 查询所有图书信息
+     * @param start 查询数据起始
+     * @param limit 查询数据量
+     * @return 返回一页图书信息
      */
-    public List<Book> selectBooks(){
+    public List<Book> selectBooks(int start,int limit){
         List<Book> bookList = null;
         try {
-            String sql = "SELECT book_id bookId, book_name bookName, book_author bookAuthor, book_price bookPrice, book_img_path bookImaPath, book_desc bookDesc, book_stock bookStock, book_type bookType FROM books";
+            String sql = "SELECT book_id bookId, book_name bookName, book_author bookAuthor, book_price bookPrice, book_img_path bookImaPath, book_desc bookDesc, book_stock bookStock, book_type bookType FROM books limit ?,? ";
             QueryRunner queryRunner = new QueryRunner(DruidUtils.getDataSource());
-            bookList = (List<Book>) queryRunner.query(sql, new BeanHandler<Book>(Book.class));
+            bookList = (List<Book>) queryRunner.query(sql, new BeanHandler<Book>(Book.class),start,limit);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return bookList;
     }
+
+    /**
+     * 查询总记录数
+     * @return 返回总记录数
+     */
+    public long selectBookCount(){
+        long count=0;
+        try{
+        String sql="select count(1) from books";
+        QueryRunner queryRunner=new QueryRunner(DruidUtils.getDataSource());
+        count=queryRunner.query(sql,new ScalarHandler<Long>());
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
 
     /**
      * 根据图书ID删除一个图书信息
