283d6a424daf24c00967c07d005f4ef306533bfc
suxss <2007402076@stu.suda.edu.cn>
2022-05-07 20:44:46 +0800

    create book-add page

--- web/js/login.js
+++ web/js/login.js
@@ -15,7 +15,7 @@ $(function () {
             beforeSend: function () {
                 $('#loginBtn').attr('disabled', true);
                 if (username == '' || password == '' || captcha == '') {
-                    Message('warning','请填写完整信息');
+                    Message('warning', '请填写完整信息');
                     $('#loginBtn').attr('disabled', false);
                     return false;
                 }
