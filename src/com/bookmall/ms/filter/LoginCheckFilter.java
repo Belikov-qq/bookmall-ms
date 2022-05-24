6ccc162e4a8b377b09da4ab9b496a7e6242e7e36
suxss <2007402076@stu.suda.edu.cn>
2022-05-24 18:48:33 +0800

    Update LoginCheckFilter.java

--- src/com/bookmall/ms/filter/LoginCheckFilter.java
+++ src/com/bookmall/ms/filter/LoginCheckFilter.java
@@ -2,6 +2,7 @@ package com.bookmall.ms.filter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -14,14 +15,21 @@ public class LoginCheckFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String uri = ((HttpServletRequest) servletRequest).getRequestURI();
         HttpSession session = ((HttpServletRequest) servletRequest).getSession();
-        if (session.getAttribute("user") == null) {
-            String tempData = "{\"error\": -2,\"msg\":\"请先登录\"}";
-            servletResponse.setContentType("application/json;charset=UTF-8");
-            ServletOutputStream out = servletResponse.getOutputStream();
-            out.write(tempData.getBytes(StandardCharsets.UTF_8));
-            out.flush();
-            out.close();
+        boolean isGet = ((HttpServletRequest) servletRequest).getMethod().equalsIgnoreCase("GET");
+        boolean isHtml = uri.endsWith(".html");
+        if (isGet && isHtml && session.getAttribute("user") == null) {
+            ((HttpServletResponse) servletResponse).sendRedirect("bookmall_ms/login.html");
+        }else{
+            if (!isHtml && session.getAttribute("user") == null) {
+                String tempData = "{\"error\": -2,\"msg\":\"请先登录\"}";
+                servletResponse.setContentType("application/json;charset=UTF-8");
+                ServletOutputStream out = servletResponse.getOutputStream();
+                out.write(tempData.getBytes(StandardCharsets.UTF_8));
+                out.flush();
+                out.close();
+            }
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
